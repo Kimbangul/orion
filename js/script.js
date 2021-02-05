0def223b7a478c36b7d44de6a2372213dc36de18
Kimbangul <highcolor_12@naver.com>
2021-02-05 16:06:15 +0900

    슬라이드 작업

--- js/script.js
+++ js/script.js
@@ -29,13 +29,14 @@ $(document).ready(function(){
 
     }
 
-    function mainSlide_init(){
+    function slideImg_init(){
         
         var $mainSlide = $('#slide div.img-slide');
         console.log($mainSlide);
         
         var $mainSlideList = [
-            '../img/slide/slide1_darken.png'
+            '../img/slide/slide1_darken.png',
+            '../img/slide/slide2.jpg'
         ];
 
         $mainSlide.each(function (index, item){
@@ -48,7 +49,34 @@ $(document).ready(function(){
     }
 
 
+    function slickSlide_init(){
+
+
+        $('section#slide > div.con').slick({
+            slide: '.img-slide',
+            
+            infinite: true,
+            fade: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed : 1000,
+            arrows : true, 	
+            prevArrow : "<button type='button' class='slick-prev'><img src='img/arrow.png' alt='이전 슬라이드로'></button>",		// 이전 화살표 모양 설정
+			nextArrow : "<button type='button' class='slick-next'><img src='img/arrow.png' alt='다음 슬라이드로'></button>",
+            dots : true, 	
+			autoplay : true,			
+			autoplaySpeed : 5000, 		
+			pauseOnHover : false,		
+			vertical : false,			
+			dotsClass : "slick-dots", 	
+			draggable : true
+
+        });
+    }
+
+
     naviMenu_init();
-    mainSlide_init();
+    slideImg_init();
+    slickSlide_init();
 
 });
